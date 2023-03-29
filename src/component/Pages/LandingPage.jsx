c54c6d5833e6a4dda57763ee4015695b71406f23
Edward Ezekiel-Hart <115160391+Eddy-zik@users.noreply.github.com>
2023-03-29 10:53:05 +0100

    login/signup pages

--- src/component/Pages/LandingPage.jsx
+++ src/component/Pages/LandingPage.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import "../../styles/Landing.scss";
-import { Navbar } from '../Navbar';
-import { Footer } from '../Footer'
 
 const LandingPage = () => {
     return (
-        <div>
-            <Navbar />
-            LandingPage
-            <Footer />
-
-         </div>
+        <div>LandingPage</div>
     )
 }
 
