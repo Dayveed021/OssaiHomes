1e44023ee3ba53dd1e078ab74a73171248ba118e
Dayveed021 <boskey021@gmail.com>
2023-03-30 13:31:38 +0100

    Updated routes in HomePages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import '../src/styles/Navbar.scss';
-import { Footer } from './component/Footer';
-import { Navbar } from './component/Navbar';
-import { Buyy } from './component/Pages/Buyy';
-import LandingPage from './component/Pages/LandingPage';
-import List from './component/Pages/List';
-import MyAgent from './component/Pages/MyAgent';
-import MyList from './component/Pages/MyList';
+import HomePages from "./component/HomePages";
 
 const App = () => {
-
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" index element={<LandingPage />} />
-        <Route path="/buy" element={<Buyy />} />
-        <Route path="/list" element={<List />} />
-        <Route path="/mylist" element={<MyList />} />
-        <Route path="/myagent" element={<MyAgent />} />
-      </Routes>
-      <Footer />
+      <HomePages />
     </div>
   );
 };
