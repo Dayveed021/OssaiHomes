1b58fd33c5dddd75fc6949b7f7950177aac74c09
Ossai Lucky <luckyblaqy@gmail.com>
2023-05-27 20:15:38 +0100

    resolve the conflict

--- src/component/Pages/admin/reusables/FilterSearch.jsx
+++ src/component/Pages/admin/reusables/FilterSearch.jsx
@@ -1,22 +1,20 @@
-import React from "react";
+import React from 'react'
 import "./reusables.scss";
 
-const FilterSearch = ({ background, marginBlock, marginInline }) => {
-  return (
-    <div
-      className="search-filter"
-      style={{ marginBlock: marginBlock, marginInline }}
-    >
-      <div className="search" style={{ backgroundColor: background }}>
-        <img src="../../flaticons/search1.png" alt="" />
-        <input type="search" placeholder="Search" />
-      </div>
-      <div className="filter" style={{ backgroundColor: background }}>
-        <h4>Filter</h4>
-        <img src="../../flaticons/menu2.png" alt="" />
-      </div>
-    </div>
-  );
-};
 
-export default FilterSearch;
+const FilterSearch = ({background, marginBlock, marginInline}) => {
+  return (
+    <div className="search-filter" style={{marginBlock:marginBlock, marginInline}}>
+        <div className="search" style={{backgroundColor:background}}>
+            <img src="../../flaticons/search1.png" alt="" />
+            <input type="search" placeholder="Search" />
+        </div>
+        <div className="filter" style={{backgroundColor:background}}>
+            <h4>Filter</h4>
+            <img src="../../flaticons/menu2.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default FilterSearch
