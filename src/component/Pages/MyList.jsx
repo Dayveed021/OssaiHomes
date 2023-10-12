f13be6b4c208961c1a36de85e13f87512cf7eb62
Ossai Lucky <luckyblaqy@gmail.com>
2023-10-12 15:12:38 +0100

    styled search result page

--- src/component/Pages/MyList.jsx
+++ src/component/Pages/MyList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "../../styles/MyList.css";
-import { PropertyData } from "../Data/Data_1";
+import "../../styles/MyList.scss";
 import { Navbar } from "../Navbar";
 import { Footer } from "./Footer";
 
@@ -8,102 +7,296 @@ function MyList() {
   return (
     <>
       <Navbar />
-      <div>
-        <nav className="searchNav">
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-            integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-          />
+      <div className="search-listing">
+        <div className="filter-search">
+          <input type="text"  placeholder="Location"/>
+          <button className="btn-1" >Price <img  src="flaticons/expand-button3.png" alt="" /></button>
+          <button className="btn-2" >Category <img src="flaticons/expand-button3.png" alt="" /></button>
+          <button className="btn-3" >Type  <img src="flaticons/expand-button3.png" alt="" /></button>
+          <button className="btn-4" >Bedrooms <img src="flaticons/expand-button3.png" alt="" /></button>
 
-          <input
-            typeof="search"
-            placeholder="Location"
-            className="search_bar"
-          />
-          <ul>
-            <li className="search_bar-s">
-              Price <i class="fa fa-angle-down" aria-hidden="true"></i>
-            </li>
-            <li className="search_bar-b">
-              Category <i class="fa fa-angle-down" aria-hidden="true"></i>
-            </li>
-            <li className="search_bar-s">
-              Type <i class="fa fa-angle-down" aria-hidden="true"></i>
-            </li>
-            <li className="search_bar-b">
-              Bedrooms <i class="fa fa-angle-down" aria-hidden="true"></i>
-            </li>
-          </ul>
-        </nav>
-        <div className="main_search">
-          <h1>
-            Showing Listing in "<span>Warri</span>" in Nigeria
-          </h1>
-          <p>
-            Pages 1/<span>200</span>:<span>400</span>properties available
-          </p>
-          <div className="sort_box">
-            Sort by: Default <i class="fa fa-angle-down" aria-hidden="true"></i>
-          </div>
-          <div>
-            {PropertyData.map((box) => {
-              return (
-                <div>
-                  <div className="prop-container">
-                    <div>
-                      <img
-                        className="img"
-                        src={box.img_path}
-                        alt=""
-                        width={200}
-                        height={200}
-                      />
-                    </div>
-                    <h2>{box.name}</h2>
-                    <p className="text">{box.desc}</p>
-                    <p className="loc">{box.location}</p>
-                    <br />
-                    <p>
-                      <span className="amount">{box.amount}</span>/month
-                    </p>
-                    <div className="num">call</div>
-                    <div className="foot">
-                      <div>guest toilet</div>
-                      <div>Bedrooms</div>
-                      <div>bathrooms</div>
-                      {/* favourite icon */}
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
         </div>
 
-        <section>
-          <div className="ASF">
-            <h2>Advanced search filter</h2>
-            <input placeholder="Location" />
-            <input value="Category" />
-            <input content="Category" />
-            <input content="Category" />
-            <input content="Category" />
-            <input content="Category" />
-            <input content="Category" />
-            <input content="Category" />
+        <h2> Showing Listings "Warri" in Nigeria</h2>
+
+        <div className="search-result">
+          <div className="search-listing">
+            <div className="heading-button">
+              <p>Page 1 / 200: 400 properties available </p>
+              <button>Sort by: Default <img  src="flaticons/expand-button3.png" alt="" /> </button>
+            </div>
+            <div className="listing-details">
+              <div className="img-left">
+                <img src="pictures/Rectangle-244.png" alt="property-img" />
+              </div>
+              <div className="details-right">
+                <h3>Ossai Agent</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.......
+                </p>
+                <h4>Warri Delta State</h4>
+                <div className="price-call-btn">
+                  <p> <span>90000000</span>/month</p>
+                  <button><img src="pictures/telephone-call-1.png" alt="" /> <span>Call</span></button>
+                </div>
+                <div className="facilaties">
+                  <div className="toilet">
+                    <img src="pictures/water-closet-1.png" alt="" /> 1 guest toilet
+                  </div>
+                  <div className="bedroom">
+                    <img src="pictures/double-bed-1.png" alt="" /> 6 bedrooms
+                  </div>
+                  <div className="bathroom">
+                    <img src="pictures/bath-1.png" alt="" /> 6 bathrooms
+                  </div>
+                  <div className="heart">
+                    <img src="pictures/heart-3.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="listing-details">
+              <div className="img-left">
+                <img src="pictures/Rectangle-242.png" alt="property-img" />
+              </div>
+              <div className="details-right">
+                <h3>Ossai Agent</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.......
+                </p>
+                <h4>Warri Delta State</h4>
+                <div className="price-call-btn">
+                  <p> <span>90000000</span>/month</p>
+                  <button><img src="pictures/telephone-call-1.png" alt="" /> <span>Call</span></button>
+                </div>
+                <div className="facilaties">
+                  <div className="toilet">
+                    <img src="pictures/water-closet-1.png" alt="" /> 1 guest toilet
+                  </div>
+                  <div className="bedroom">
+                    <img src="pictures/double-bed-1.png" alt="" /> 6 bedrooms
+                  </div>
+                  <div className="bathroom">
+                    <img src="pictures/bath-1.png" alt="" /> 6 bathrooms
+                  </div>
+                  <div className="heart">
+                    <img src="pictures/heart-3.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="listing-details">
+              <div className="img-left">
+                <img src="pictures/Rectangle-243.png" alt="property-img" />
+              </div>
+              <div className="details-right">
+                <h3>Ossai Agent</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.......
+                </p>
+                <h4>Warri Delta State</h4>
+                <div className="price-call-btn">
+                  <p> <span>90000000</span>/month</p>
+                  <button><img src="pictures/telephone-call-1.png" alt="" /> <span>Call</span></button>
+                </div>
+                <div className="facilaties">
+                  <div className="toilet">
+                    <img src="pictures/water-closet-1.png" alt="" /> 1 guest toilet
+                  </div>
+                  <div className="bedroom">
+                    <img src="pictures/double-bed-1.png" alt="" /> 6 bedrooms
+                  </div>
+                  <div className="bathroom">
+                    <img src="pictures/bath-1.png" alt="" /> 6 bathrooms
+                  </div>
+                  <div className="heart">
+                    <img src="pictures/heart-3.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="listing-details">
+              <div className="img-left">
+                <img src="pictures/Rectangle-247.png" alt="property-img" />
+              </div>
+              <div className="details-right">
+                <h3>Ossai Agent</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.......
+                </p>
+                <h4>Warri Delta State</h4>
+                <div className="price-call-btn">
+                  <p> <span>90000000</span>/month</p>
+                  <button><img src="pictures/telephone-call-1.png" alt="" /> <span>Call</span></button>
+                </div>
+                <div className="facilaties">
+                  <div className="toilet">
+                    <img src="pictures/water-closet-1.png" alt="" /> 1 guest toilet
+                  </div>
+                  <div className="bedroom">
+                    <img src="pictures/double-bed-1.png" alt="" /> 6 bedrooms
+                  </div>
+                  <div className="bathroom">
+                    <img src="pictures/bath-1.png" alt="" /> 6 bathrooms
+                  </div>
+                  <div className="heart">
+                    <img src="pictures/heart-3.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="listing-details">
+              <div className="img-left">
+                <img src="pictures/Rectangle-246.png" alt="property-img" />
+              </div>
+              <div className="details-right">
+                <h3>Ossai Agent</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.......
+                </p>
+                <h4>Warri Delta State</h4>
+                <div className="price-call-btn">
+                  <p> <span>90000000</span>/month</p>
+                  <button><img src="pictures/telephone-call-1.png" alt="" /> <span>Call</span></button>
+                </div>
+                <div className="facilaties">
+                  <div className="toilet">
+                    <img src="pictures/water-closet-1.png" alt="" /> 1 guest toilet
+                  </div>
+                  <div className="bedroom">
+                    <img src="pictures/double-bed-1.png" alt="" /> 6 bedrooms
+                  </div>
+                  <div className="bathroom">
+                    <img src="pictures/bath-1.png" alt="" /> 6 bathrooms
+                  </div>
+                  <div className="heart">
+                    <img src="pictures/heart-3.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="listing-details">
+              <div className="img-left">
+                <img src="pictures/Rectangle-245.png" alt="property-img" />
+              </div>
+              <div className="details-right">
+                <h3>Ossai Agent</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu.......
+                </p>
+                <h4>Warri Delta State</h4>
+                <div className="price-call-btn">
+                  <p> <span>90000000</span>/month</p>
+                  <button><img src="pictures/telephone-call-1.png" alt="" /> <span>Call</span></button>
+                </div>
+                <div className="facilaties">
+                  <div className="toilet">
+                    <img src="pictures/water-closet-1.png" alt="" /> 1 guest toilet
+                  </div>
+                  <div className="bedroom">
+                    <img src="pictures/double-bed-1.png" alt="" /> 6 bedrooms
+                  </div>
+                  <div className="bathroom">
+                    <img src="pictures/bath-1.png" alt="" /> 6 bathrooms
+                  </div>
+                  <div className="heart">
+                    <img src="pictures/heart-3.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
-            <input type="checkbox" />
+          <div className="search-input">
+                <div className="advanced-search">
+                      <h3>Advanced Search filter</h3>
+                      <form action="">
+                        <div className="location">
+                          <input type="text"  placeholder="Location"/>
+                        </div>
+
+                        <div className="category">
+                          <p>Category</p>
+                          <select id="category" name="category">
+                          <option></option>
+                          <option value="apartment">Apartment</option>
+                          <option value="duplex">Duplex</option>
+                        </select>       
+                        </div>
+
+                        <div className="price">
+                          <div className="min-price">
+                            <input type="text"  placeholder="Min Price"/>
+                            <select id="category" name="category">
+                            <option value=""></option>
+                            <option value="1000">1000</option>
+                            <option value="2000">2000</option>
+                            </select>   
+                          </div>
+                          <div className="min-price">
+                            <input type="text"  placeholder="Max Price"/>
+                            <select id="category" name="category">
+                              <option value=""></option>
+                            <option value="10000">10000</option>
+                            <option value="20000">20000</option>
+                            </select>   
+                          </div>
+                        </div>
+
+                        <div className="price">
+                          <div className="min-price">
+                            <input type="text"  placeholder="Type"/>
+                            <select id="category" name="category">
+                            <option value=""></option>
+                            <option value="apartment">apartment</option>
+                            <option value="duplex">duplex</option>
+                            </select>   
+                          </div>
+                          <div className="min-price">
+                            <input type="number"  placeholder="Furnish"/>
+                            <select id="category" name="category">
+                            <option value=""></option>
+                            <option value="fully">Fully</option>
+                            <option value="partially">Partially</option>
+                            </select>   
+                          </div>
+                        </div>
+                        <div className="price">
+                          <div className="min-price">
+                            <input type="text"  placeholder="Bedrooms"/>
+                            <select id="category" name="category">
+                            <option value=""></option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            </select>   
+                          </div>
+                          <div className="min-price">
+                            <input type="text"  placeholder="Time listed"/>
+                            <select id="category" name="category">
+                            <option value=""></option>
+                            <option value="8">8am</option>
+                            <option value="9">9am</option>
+                            </select>   
+                          </div>
+                        </div>
+
+                        <div className="keywords">
+                          <input type="search" placeholder="keywords e.g pool,garage" />
+                        </div>
+
+
+                      </form>
+                </div>
+
+                <div className="Nearby">
+
+                </div>
           </div>
-        </section>
+        </div>
+       
+        
+
+      
       </div>
       <Footer />
     </>
